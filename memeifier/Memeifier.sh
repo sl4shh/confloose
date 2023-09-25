99f1d3e91b8e86373d9cba5f46fb88a738360b71
sl4.sh <43612518+sl4shh@users.noreply.github.com>
2023-09-25 18:25:43 +0200

    Update Memeifier.sh

--- memeifier/Memeifier.sh
+++ memeifier/Memeifier.sh
@@ -1,1 +1,19 @@
+cp ./i3l.png ~/afs/
+cp tehc.jpg ~/afs/
 
+# Modifs du bashrc
+feh --bg-scale ~/afs/tehc.jpg
+setxkbmap fr bepo
+echo "feh --bg-scale ~/afs/tehc.jpg" >> ~/.bashrc
+echo "alias i3lock='i3lock -i ~/afs/i3l.png'" >> ~/.bashrc
+echo "alias emacs='vim'" >> ~/.bashrc
+echo "alias nano='vim'" >> ~/.bashrc
+echo "alias firefox='firefox https://www.youtube.com/watch?v=dQw4w9WgXcQ'" >> ~/.bashrc
+echo "alias gedit='vim'" >> ~/.bashrc
+echo "setxkbmap fr bepo" >> ~/.bashrc
+
+# Terminaux en blanc
+echo "printf \"\033[47m\033[30m\"" >> ~/.bashrc"
+
+clear
+exit N
